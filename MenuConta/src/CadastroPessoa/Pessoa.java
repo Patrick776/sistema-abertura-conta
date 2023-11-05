e325285ec3a818881a850deeddb3cc81f462b3c6
Patrick776 <75255193+Patrick776@users.noreply.github.com>
2023-11-05 11:51:29 -0300

    primeiro passo java menu

---
+++ MenuConta/src/CadastroPessoa/Pessoa.java
@@ -0,0 +1,39 @@
+package CadastroPessoa;
+
+public class Pessoa {
+	
+	//Classe Pessoa
+	private String nome;
+	private int cpf;
+	private String endereco;
+	private String telefone;
+	
+	//Contrutor, getters e setters
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getCpf() {
+		return cpf;
+	}
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+		
+
+}
