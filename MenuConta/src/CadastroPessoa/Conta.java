00b5e6f505f497d3bddd34d9cfe8cdda2475ad6c
edudalmazo <eduardodalmazo@yahoo.com.br>
2023-11-06 08:12:10 -0300

    Adicionando o arqruivo como Conta.java

---
+++ MenuConta/src/CadastroPessoa/Conta.java
@@ -0,0 +1,31 @@
+package CadastroPessoa
+
+public class Conta {
+    private int numero;
+    private double saldo;
+
+    private Conta (int nunmero) {
+        this.numero = numero;
+        this.saldo = 0;
+    }
+
+    public int getNumero(){
+        return numero;
+    }
+
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public void depositar(double valor) {
+        saldo += valor;
+    }
+
+    public void sacar(double valor){
+        saldo -= valor;
+    }
+
+    public String toString(){
+        return numero + "saldo: " + saldo;
+    }
+}
